7d6498aa221ee3bada1639632576062f089c6c79
vxtruog <truongvx.2001@gmail.com>
2026-06-08 21:43:12 +0700

    Update server.c

--- server.c
+++ server.c
@@ -16,3 +16,25 @@
 #define PORT "3490"  // cổng mà người dùng kết nối đến
 #define BACKLOG 10   // số kết nối đang chờ xử lý tối đa trong hàng đợi
 
+void sigchld_handler(int s)
+{
+  void(s);                    // tránh cảnh báo biến không được sử dụng
+  int saved_errno = errno;    // waitpid() có thể ghi đè nên errno, nên chúng ta sẽ lưu trữ và phục hồi nó sau
+  while(waitpid(-1, NULL, WNOHANG) > 0);
+  errno = saved_errno;
+}
+
+void *get_in_addr(struct sockaddr *sa)
+{
+  if (sa->sa_family == AF_INET)
+  {
+    return &(((struct sockaddr_in*)sa)->sin_addr);
+  }
+
+  return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+
+
+
+      
