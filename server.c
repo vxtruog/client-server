35f62337959b9bdf1b05540e8166634d90f9b3a4
vxtruog <truongvx.2001@gmail.com>
2026-06-11 05:21:49 +0700

    Update server.c

--- server.c
+++ server.c
@@ -79,6 +79,49 @@ int main(void)
 
   freeaddrinfo(servinfo);
 
+  if(p == NULL) {
+    fprintf(stderr, "server: failed to bind\n");
+    exit(1);
+  }
+
+  if(listen(sockfd, BACKLOG) == -1) {
+    perror("listen");
+    exit(1);
+  }
+
+  sa.sa_handler = sigchld_handler; // reap all dead processes
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = SA_RESTART;
+  if(sigaction(SIGCHLD, &sa, NULL) == -1) {
+    perror("sigaction");
+    exit(1);
+  }
+
+  printf("server: waiting for connections...\n");
+
+  while(1) {  // main accept() loop
+    sin_size = sizeof their_addr;
+    new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+    if(new_fd == -1) {
+      perror("accept");
+      continue;
+    }
+
+    inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr), s, sizeof s);
+    printf("server: got connection from %s\n", s);
+
+    if(!fork()) { // this is the child process
+      close(sockfd); // child doesn't need the listener
+      if(send(new_fd, "Hello, world!", 13, 0) == -1)
+        perror("send");
+      close(new_fd);
+      exit(0);
+    }
+    close(new_fd);  // parent doesn't need this
+  }
+  
+  return 0;
+}
   
 
   
