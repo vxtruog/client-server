b2172c50c448c99e078c271331fb7ab54a8cfba8
vxtruog <truongvx.2001@gmail.com>
2026-06-22 01:36:26 +0700

    Update server.c

--- server.c
+++ server.c
@@ -1,132 +1,33 @@
-// a stream socket server demo
-
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <errno.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
 #include <arpa/inet.h>
-#include <sys/wait.h>
-#include <signal.h>
 
-#define PORT "3490"  // cổng mà người dùng kết nối đến
-#define BACKLOG 10   // số kết nối đang chờ xử lý tối đa trong hàng đợi
-
-void sigchld_handler(int s)
+int main()
 {
-  (void)s;                    // tránh cảnh báo biến không được sử dụng
-  int saved_errno = errno;    // waitpid() có thể ghi đè nên errno, nên chúng ta sẽ lưu trữ và phục hồi nó sau
-  while(waitpid(-1, NULL, WNOHANG) > 0);
-  errno = saved_errno;
-}
-
-void *get_in_addr(struct sockaddr *sa)
-{
-  if (sa->sa_family == AF_INET)
+  // các biến
+  int serverSocketFd, clientSocketFd;
+  struct sockaddr_in serverAddress, clientAddress;
+  int clientAddressLength;
+  
+  // tạo socket
+  serverSocketFd = socket(AF_INET, SOCK_STREAM, 0);
+  if(serverSocketFd == -1)
   {
-    return &(((struct sockaddr_in*)sa)->sin_addr);
-  }
-
-  return &(((struct sockaddr_in6*)sa)->sin6_addr);
-}
-
-int main(void)
-{
-  // nghe trên sock_fd, kết nối mới trên new_fd
-  int sock_fd, new_fd;
-  struct addrinfo hints, *servinfo, *p;
-  struct sockaddr_storage their_addr;
-  socklen_t sin_size;
-  struct sigaction sa;
-  int yes = 1;
-  char s[INET6_ADDRSTRLEN];
-  int rv;
-
-  memset(&hints, 0, sizeof hints);
-  hints.ai_family = AF_INET;
-  hints.ai_socktype = SOCK_STREAM;
-  hints.ai_flags = AI_PASSIVE;      // sử dụng IP của tôi
-
-  if((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
-    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-    return 1;
-  }
-
-  //
-  for(p = servinfo; p != NULL; p = p->ai_next) {
-    if((sock_fd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
-      perror("server: socket");
-      continue;
-    }
-
-    if(setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
-      perror("setsockopt");
-      exit(1);
-    }
-
-    if(bind(sock_fd, p->ai_addr, p->ai_addrlen) == -1) {
-      close(sock_fd);
-      perror("server: bind");
-      continue;
-    }
-
-    break;
-  }
-
-  freeaddrinfo(servinfo);
-
-  if(p == NULL) {
-    fprintf(stderr, "server: failed to bind\n");
+    printf("failed to create a socket\n");
     exit(1);
   }
-
-  if(listen(sock_fd, BACKLOG) == -1) {
-    perror("listen");
-    exit(1);
-  }
-
-  sa.sa_handler = sigchld_handler; // reap all dead processes
-  sigemptyset(&sa.sa_mask);
-  sa.sa_flags = SA_RESTART;
-  if(sigaction(SIGCHLD, &sa, NULL) == -1) {
-    perror("sigaction");
-    exit(1);
-  }
-
-  printf("server: waiting for connections...\n");
-
-  while(1) {  // main accept() loop
-    sin_size = sizeof their_addr;
-    new_fd = accept(sock_fd, (struct sockaddr *)&their_addr, &sin_size);
-    if(new_fd == -1) {
-      perror("accept");
-      continue;
-    }
-
-    inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr), s, sizeof s);
-    printf("server: got connection from %s\n", s);
-
-    if(!fork()) { // this is the child process
-      close(sock_fd); // child doesn't need the listener
-      if(send(new_fd, "Hello, world!", 13, 0) == -1)
-        perror("send");
-      close(new_fd);
-      exit(0);
-    }
-    close(new_fd);  // parent doesn't need this
+  else
+  {
+    printf("socket: %d\n", serverSocketFd);
   }
   
+  // liên kết
+  
+  // lắng nghe
+  // chấp nhận
+  // giao tiếp
+  // đóng
   return 0;
 }
-  
-
-  
-
-
-
-
-      
