d6ad06f109d6cbfccb93b255499a1426a906a0f6
vxtruog <truongvx.2001@gmail.com>
2026-06-22 21:12:53 +0700

    Update client.c

--- client.c
+++ client.c
@@ -1,92 +1,50 @@
-
-// client.c -- a stream socket client demo
-
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <errno.h>
-#include <string.h>
-#include <netdb.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
 #include <arpa/inet.h>
 
-#define PORT "3490" // the port client will be connecting to 
-
-#define MAXDATASIZE 100 // max number of bytes we can get at once 
-
-// get sockaddr, IPv4 or IPv6:
-void *get_in_addr(struct sockaddr *sa)
+int main()
 {
-	if (sa->sa_family == AF_INET) {
-		return &(((struct sockaddr_in*)sa)->sin_addr);
+	// tên các biến
+	int socketFd;
+	struct sockaddr_in serverAddress;
+	
+	// tạo socket
+	socketFd = socket(AF_INET, SOCK_STREAM, 0);
+	if(socketFd == -1)
+	{
+		printf("failed to create a socket\n");
+		exit(1);
 	}
-
-	return &(((struct sockaddr_in6*)sa)->sin6_addr);
-}
-
-int main(int argc, char *argv[])
-{
-  int sockfd, numbytes;  
-	char buf[MAXDATASIZE];
-	struct addrinfo hints, *servinfo, *p;
-	int rv;
-	char s[INET6_ADDRSTRLEN];
-
-	if(argc != 2) {
-    fprintf(stderr,"usage: client hostname\n");
-    exit(1);
+	else
+	{
+		printf("socket : %d\n", socketFd);
 	}
-
-	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-
-	if((rv = getaddrinfo(argv[1], PORT, &hints, &servinfo)) != 0) {
-    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-		return 1;
+	
+	// kết nối
+	bzero(&serverAddress, sizeof(serverAddress));
+	serverAddress.sin_family = AF_INET;
+	serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
+	serverAddress.sin_port = htons(4444);
+	if(connect() != 0)
+	{
+		printf("failed to connection\n");
+		exit(1);
 	}
-
-	// loop through all the results and connect to the first we can
-	for(p = servinfo; p != NULL; p = p->ai_next) {
-		if((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
-			perror("client: socket");
-			continue;
-		}
-    
-    inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr), s, sizeof s);
-    printf("client: attempting connection to %s\n", s);
-
-		if(connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-      perror("client: connect");
-			close(sockfd);
-			continue;
-		}
-
-		break;
+	else
+	{
+		printf("socket : %d\n", socketFd);
 	}
-
-	if(p == NULL) {
-		fprintf(stderr, "client: failed to connect\n");
-		return 2;
-	}
-
-	inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr), s, sizeof s);
-	printf("client: connected to %s\n", s);
-
-	freeaddrinfo(servinfo); // all done with this structure
-
-	if((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
-    perror("recv");
-    exit(1);
-	}
-
-	buf[numbytes] = '\0';
-
-	printf("client: received '%s'\n",buf);
-
-	close(sockfd);
+	
+	// đọc
+	char buf[128];
+	read(socketFd, buf, 128);
+	printf("received : %s\n", buf);
+	
+	// đóng
+	close(socketFd);
+	printf("socket closed\");
 
 	return 0;
 }
