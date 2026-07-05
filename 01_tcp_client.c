d8e2cc1ee6e553f30b6cdf3fc6f4116bd4c87244
Vũ Xuân Trường <truongvx.2001@gmail.com>
2026-07-05 15:56:33 +0700

    Update 01_tcp_client.c

--- 01_tcp_client.c
+++ 01_tcp_client.c
@@ -2,6 +2,10 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
 
 int main()
@@ -9,13 +13,14 @@ int main()
 	// variables
 	int socketFd;
 	struct sockaddr_in serverAddress;
+	ssize_t receivedBytes;
 	
 	// create socket
 	socketFd = socket(AF_INET, SOCK_STREAM, 0);
 	if(socketFd == -1)
 	{
-		printf("failed to create a socket\n");
-		exit(1);
+		perror("socket");
+		exit(EXIT_FAILURE);
 	}
 	else
 	{
@@ -27,10 +32,10 @@ int main()
 	serverAddress.sin_family = AF_INET;
 	serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
 	serverAddress.sin_port = htons(4444);
-	if(connect(socketFd, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) != 0)
+	if(connect(socketFd, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1)
 	{
-		printf("failed to connection\n");
-		exit(1);
+		perror("connect");
+		exit(EXIT_FAILURE);
 	}
 	else
 	{
@@ -39,8 +44,22 @@ int main()
 	
 	// comminucation
 	char buf[128];
-	recv(socketFd, buf, 128, 0);
-	printf("received : %s\n", buf);
+	receivedBytes = recv(socketFd, buf, sizeof(buf) - 1, 0);
+	if(receiveBytes == -1)
+	{
+		perror("recv");
+		exit(EXIT_FAILURE);
+	}
+	else if(receiveBytes == 0)
+	{
+		printf("server disconnected\n");
+	}
+	else
+	{
+		buf[receiveBytes] = '\0';
+		printf("received %zd bytes\n", receiveBytes);
+		printf("received : %s\n", buf);
+	}
 	
 	// close
 	close(socketFd);
