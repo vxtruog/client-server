d0f05871098a9caa6817e666f10475893bb8a8d3
Vũ Xuân Trường <truongvx.2001@gmail.com>
2026-07-05 21:34:18 +0700

    Update 02_echo_tcp_server.c

--- 02_echo_tcp_server.c
+++ 02_echo_tcp_server.c
@@ -10,76 +10,79 @@
 
 int main(int argc, char *argv[])
 {
-  // variables
-  int serverSocketFd, clientSocketFd;
-  struct sockaddr_in serverAddress, clientAddress;
-  socklen_t clientAddressLength = sizeof(clientAddress);
-  ssize_t sentBytes, receivedBytes;
-
-  if(argc != 2)
-  {
-    fprintf(stderr, "usage: %s <port>\n", argv[0]);
-    exit(EXIT_FAILURE);
-  }
-  
-  // create socket
-  serverSocketFd = socket(AF_INET, SOCK_STREAM, 0);
-  if(serverSocketFd == -1)
-  {
-    perror("socket");
-    exit(EXIT_FAILURE);
-  }
-  else
-  {
-    printf("socket: %d\n", serverSocketFd);
-  }
-  
-  // bind
-  memset(&serverAddress, 0, sizeof(serverAddress));
-  serverAddress.sin_family = AF_INET;
-  serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
-  serverAddress.sin_port = htons(atoi(argv[1]));
-  if(bind(serverSocketFd, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) == -1)
-  {
-    perror("bind");
-    exit(EXIT_FAILURE);
-  }
-  else
-  {
-    printf("bound\n");
-  }
-  
-  // listen
-  if(listen(serverSocketFd, 1) == -1)
-  {
-    perror("listen");
-    exit(EXIT_FAILURE);
-  }
-  else
-  {
-    printf("listening...\n");
-  }
-  
-  // accept
-  memset(&clientAddress, 0, sizeof(clientAddress));
-  clientSocketFd = accept(serverSocketFd, (struct sockaddr *)&clientAddress, &clientAddressLength);
-  if(clientSocketFd == -1)
-  {
-    perror("accept");
-    exit(EXIT_FAILURE);
-  }
-  else
-  {
-    printf("accepted\n");
-  }
-  
-  // comminucation
-  char buf[128];
-  char res[128];
-  while(1)
-  {
-    // receive
-    receivedBytes = recv(clientSocketFd, buf, sizeof(buf) - 1, 0);
+	// variables
+	int serverSocketFd, clientSocketFd;
+	struct sockaddr_in serverAddress, clientAddress;
+	socklen_t clientAddressLength = sizeof(clientAddress);
+	ssize_t sentBytes, receivedBytes;
+	
+	if(argc != 2)
+	{
+		fprintf(stderr, "usage: %s <port>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	
+	// create socket
+	serverSocketFd = socket(AF_INET, SOCK_STREAM, 0);
+	if(serverSocketFd == -1)
+	{
+		perror("socket");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		printf("socket: %d\n", serverSocketFd);
+	}
+	
+	int opt = 1;
+	setsockopt(serverSocketFd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+	
+	// bind
+	memset(&serverAddress, 0, sizeof(serverAddress));
+	serverAddress.sin_family = AF_INET;
+	serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+	serverAddress.sin_port = htons(atoi(argv[1]));
+	if(bind(serverSocketFd, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) == -1)
+	{
+		perror("bind");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		printf("bound\n");
+	}
+	
+	// listen
+	if(listen(serverSocketFd, 1) == -1)
+	{
+		perror("listen");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		printf("listening...\n");
+	}
+	
+	// accept
+	memset(&clientAddress, 0, sizeof(clientAddress));
+	clientSocketFd = accept(serverSocketFd, (struct sockaddr *)&clientAddress, &clientAddressLength);
+	if(clientSocketFd == -1)
+	{
+		perror("accept");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		printf("accepted\n");
+	}
+	
+	// comminucation
+	char buf[128];
+	char res[128];
+	while(1)
+	{
+		// receive
+		receivedBytes = recv(clientSocketFd, buf, sizeof(buf) - 1, 0);
 		if(receivedBytes == -1)
 		{
 			perror("recv");
@@ -96,26 +99,26 @@ int main(int argc, char *argv[])
 			printf("received %zd bytes\n", receivedBytes);
 			printf("received: %s\n", buf);
 		}
-
-    // process
-    snprintf(res, sizeof(res), "%s", buf);
-
-    // send
-    sentBytes = send(clientSocketFd, res, strlen(res), 0);
-    if(sentBytes == -1)
-    {
-      perror("send");
-      break;
-    }
-    printf("sent %zd bytes\n", sentBytes);
-    printf("message: \"%s\"\n\n", res);
-  }
-
-  // close
-  close(clientSocketFd);
-  printf("client socket closed\n");
-  close(serverSocketFd);
-  printf("server socket closed\n");
-  
-  return 0;
+		
+		// process
+		snprintf(res, sizeof(res), "%s", buf);
+		
+		// send
+		sentBytes = send(clientSocketFd, res, strlen(res), 0);
+		if(sentBytes == -1)
+		{
+			perror("send");
+			break;
+		}
+		printf("sent %zd bytes\n", sentBytes);
+		printf("message: \"%s\"\n\n", res);
+	}
+	
+	// close
+	close(clientSocketFd);
+	printf("client socket closed\n");
+	close(serverSocketFd);
+	printf("server socket closed\n");
+	
+	return 0;
 }
